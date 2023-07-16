b9e940e6349b10c5e4b8c199ade351f0a925ce8e
Pavel Solovev <daron439@gmail.com>
2023-07-16 21:06:13 +0300

    uneeded

--- scripts/post/post-install.sh
+++ scripts/post/post-install.sh
@@ -2,3 +2,5 @@ rpm-ostree override replace https://bodhi.fedoraproject.org/updates/FEDORA-2023-
 rpm-ostree override replace xorg-x11-server-Xwayland --experimental --from repo='copr:copr.fedorainfracloud.org:solopasha:xwayland'
 
 systemctl enable fixcard.service
+systemctl disable nvidia-powerd.service
+systemctl --global enable mpd
