3bf3eb34fc531937dcad53349dc50b74fa2ad5db
Pavel Solovev <daron439@gmail.com>
2023-07-17 20:05:55 +0300

    update systemd to fix libvirt hang

--- scripts/post/post-install.sh
+++ scripts/post/post-install.sh
@@ -1,3 +1,4 @@
+rpm-ostree override replace 'https://koji.fedoraproject.org/koji/buildinfo?buildID=2234125'
 rpm-ostree override replace xorg-x11-server-Xwayland --experimental --from repo='copr:copr.fedorainfracloud.org:solopasha:xwayland'
 
 systemctl enable fixcard.service
