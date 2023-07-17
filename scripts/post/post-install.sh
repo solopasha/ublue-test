68429b56d3ca94d085974e03bb651d75fa93f5a5
Pavel Solovev <daron439@gmail.com>
2023-07-17 20:30:28 +0300

    try to workaround...

--- scripts/post/post-install.sh
+++ scripts/post/post-install.sh
@@ -1,6 +1,7 @@
-rpm-ostree override replace 'https://koji.fedoraproject.org/koji/buildinfo?buildID=2234125'
 rpm-ostree override replace xorg-x11-server-Xwayland --experimental --from repo='copr:copr.fedorainfracloud.org:solopasha:xwayland'
 
 systemctl enable fixcard.service
 systemctl disable nvidia-powerd.service
 systemctl --global enable mpd
+
+rpm-ostree override replace 'https://koji.fedoraproject.org/koji/buildinfo?buildID=2234125'
