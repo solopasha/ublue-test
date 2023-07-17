a1452924933062d42c0d82aaedb0d3d7351b9cc0
Pavel Solovev <daron439@gmail.com>
2023-07-17 11:44:23 +0300

    remove uneeded override

--- scripts/post/post-install.sh
+++ scripts/post/post-install.sh
@@ -1,4 +1,3 @@
-rpm-ostree override replace https://bodhi.fedoraproject.org/updates/FEDORA-2023-29e14b5faf
 rpm-ostree override replace xorg-x11-server-Xwayland --experimental --from repo='copr:copr.fedorainfracloud.org:solopasha:xwayland'
 
 systemctl enable fixcard.service
