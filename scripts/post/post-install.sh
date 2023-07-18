b0c8d206b2ce7dd6846c30165f5c6c9047f2b191
Pavel Solovev <daron439@gmail.com>
2023-07-18 23:27:33 +0300

    remove unneeded override
    [no ci]

--- scripts/post/post-install.sh
+++ scripts/post/post-install.sh
@@ -1,5 +1,4 @@
 rpm-ostree override replace xorg-x11-server-Xwayland --experimental --from repo='copr:copr.fedorainfracloud.org:solopasha:xwayland'
-rpm-ostree override replace https://bodhi.fedoraproject.org/updates/FEDORA-2023-e0be425f79
 
 systemctl enable fixcard.service
 systemctl disable nvidia-powerd.service
