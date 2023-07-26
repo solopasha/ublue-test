bf31f2e09660f17aa45be1d4a35df025087ee2aa
Pavel Solovev <daron439@gmail.com>
2023-07-26 11:58:30 +0300

    remove unneeded

--- scripts/post/post-install.sh
+++ scripts/post/post-install.sh
@@ -1,6 +1,6 @@
 rpm-ostree override replace xorg-x11-server-Xwayland --experimental --from repo='copr:copr.fedorainfracloud.org:solopasha:xwayland'
 rpm-ostree override replace sddm --experimental --from repo='copr:copr.fedorainfracloud.org:solopasha:sddm-test'
-rpm-ostree override replace https://bodhi.fedoraproject.org/updates/FEDORA-2023-e7c9ab3531 https://bodhi.fedoraproject.org/updates/FEDORA-2023-880d61a6ab
+rpm-ostree override replace https://bodhi.fedoraproject.org/updates/FEDORA-2023-880d61a6ab
 
 systemctl enable fixcard.service
 systemctl disable nvidia-powerd.service
